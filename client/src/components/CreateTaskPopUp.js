3167f9e1cf2e99a8455b0fa227478e18073ea3fc
Evan Kiser <kiser.evan1@gmail.com>
2020-09-14 13:55:10 -0500

    New Task Looks Better

--- client/src/components/CreateTaskPopUp.js
+++ client/src/components/CreateTaskPopUp.js
@@ -1,29 +1,66 @@
-import React, { Component } from "react";
+import React from "react";
 import '../css/createTaskPopUp.css'
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import { makeStyles } from '@material-ui/core/styles';
 
-export default class CreateTaskPopUp extends Component {
-  handleClick = () => {
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function CreateTaskPopUp(props) {
+  const classes = useStyles();
+
+  const handleClick = () => {
    this.props.toggle();
   };
 
-render() {
   return (
     <div className="modal">
     <div className="modal_content">
-      <span className="close" onClick={this.handleClick}>
+      <span className="close" onClick={handleClick}>
         &times;
       </span>
-      <form>
-        <h3>Register!</h3>
-        <label>
-          Name:
-          <input type="text" name="name" />
-        </label>
-        <br />
-        <input type="submit" />
+      <form className={classes.form} noValidate>
+          What are you trying to complete today?
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="taskTitle"
+            name="taskTitle"
+            autoFocus
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Submit
+          </Button>
       </form>
     </div>
   </div>
   );
  }
-}
