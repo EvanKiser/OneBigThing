9fa99f3fc0314021311d2acf0b8de4e59bd0ca27
Evan Kiser <kiser.evan1@gmail.com>
2020-09-17 11:10:38 -0500

    Can run front and back on same server

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -9,6 +9,7 @@ router.get('/allTasks/', async (req, res) => {
     .exec()
     .then(tasks => {
         if (tasks) {
+            console.log(tasks)
             res.status(200).json(tasks)
         } else {
             res.send("Not Tasks Found");
--- server/server.js
+++ server/server.js
@@ -27,6 +27,16 @@ app.use('/', require('./routes/index'))
 app.use('/auth', require('./routes/auth'))
 app.use('/task', require('./routes/tasks'))
 
+//  Serve static assests if in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('../client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
+    })
+}
+
 const PORT = process.env.PORT || 3001
 
 app.listen(
