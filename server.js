a273cf62587c38e9cb328c2c905f4de256bdee1c
Evan Kiser <kiser.evan1@gmail.com>
2020-09-18 12:33:48 -0500

    Heroku commmit

--- server.js
+++ server.js
@@ -29,14 +29,14 @@ app.use('/task', require('./routes/tasks'))
 //  Serve static assests if in production
 if (process.env.NODE_ENV === 'production') {
     // Set static folder
-    app.use(express.static('../client/build'));
+    app.use(express.static(' client/build'));
 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     })
 }
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3000
 
 app.listen(
     PORT, 
